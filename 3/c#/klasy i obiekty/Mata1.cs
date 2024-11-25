c3c77f80a7ddbd69a1ff3f61ce8f6963f1033136
RedDuo <goskar0808@gmail.com>
2024-11-25 14:08:50 +0100

    klasy i obiekty

---
+++ 3/c#/klasy i obiekty/Mata1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasy_i_obiekty
+{
+    internal class Mata1
+    {
+        public double Suma(double a, double b)
+        {
+            return a + b;
+        }
+        static public double Roznica(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ 3/c#/klasy i obiekty/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace klasy_i_obiekty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mata1 m1 = new Mata1();
+            int x = 0;
+            int y = 1;
+            Console.WriteLine($"{x} + {y} = "+ m1.Suma(x,y));
+            Console.WriteLine($"{x} - {y} = " + Mata1.Roznica(x, y));
+        }
+    }
+}
