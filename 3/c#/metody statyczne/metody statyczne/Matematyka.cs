b1afc5777e7a2f8696d3e4dc7d864b256b9f394e
RedDuo <goskar0808@gmail.com>
2024-12-09 14:57:51 +0100

    metody statyczne

---
+++ 3/c#/metody statyczne/metody statyczne/Matematyka.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace metody_statyczne
+{
+    public static class Matematyka
+    {
+        // 1. Metoda obliczająca silnię liczby naturalnej
+        public static long Silnia(int n)
+        {
+            if (n < 0) throw new ArgumentException("Liczba musi być naturalna (n >= 0).");
+            long wynik = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                wynik *= i;
+            }
+            return wynik;
+        }
+
+        // 2. Metoda obliczająca potęgę liczby rzeczywistej
+        public static double Potega(double podstawa, int wykladnik)
+        {
+            double wynik = 1;
+            int absWykladnik = wykladnik < 0 ? -wykladnik : wykladnik;
+            for (int i = 0; i < absWykladnik; i++)
+            {
+                wynik *= podstawa;
+            }
+            if (wykladnik < 0)
+            {
+                wynik = 1 / wynik;
+            }
+            return wynik;
+        }
+
+        // 3. Metoda sprawdzająca, czy liczba jest pierwsza
+        public static bool CzyPierwsza(int liczba)
+        {
+            if (liczba <= 1) return false;
+            for (int i = 2; i * i <= liczba; i++)
+            {
+                if (liczba % i == 0) return false;
+            }
+            return true;
+        }
+
+        // 4. Metoda sprawdzająca, czy liczba jest doskonała
+        public static bool CzyDoskonala(int liczba)
+        {
+            if (liczba <= 1) return false;
+            int sumaDzielnikow = 1;
+            for (int i = 2; i <= liczba / 2; i++)
+            {
+                if (liczba % i == 0)
+                {
+                    sumaDzielnikow += i;
+                }
+            }
+            return sumaDzielnikow == liczba;
+        }
+
+        // 5. Algorytm Euklidesa do obliczania NWD
+        public static int NWD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
---
+++ 3/c#/metody statyczne/metody statyczne/Program.cs
@@ -0,0 +1,73 @@
+﻿using metody_statyczne;
+using System;
+
+namespace metody_statyczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nWybierz opcję: ");
+                Console.WriteLine("1. Oblicz silnię");
+                Console.WriteLine("2. Oblicz potęgę");
+                Console.WriteLine("3. Sprawdź, czy liczba jest pierwsza");
+                Console.WriteLine("4. Sprawdź, czy liczba jest doskonała");
+                Console.WriteLine("5. Oblicz NWD");
+                Console.WriteLine("0. Wyjście z programu");
+
+                try
+                {
+                    byte opcja = Convert.ToByte(Console.ReadLine());
+
+                    switch (opcja)
+                    {
+                        case 1:
+                            Console.Write("Podaj liczbę naturalną: ");
+                            int n = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Silnia wynosi: " + Matematyka.Silnia(n));
+                            break;
+                        case 2:
+                            Console.Write("Podaj podstawę: ");
+                            double podstawa = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Podaj wykładnik: ");
+                            int wykladnik = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Wynik potęgowania: " + Matematyka.Potega(podstawa, wykladnik));
+                            break;
+                        case 3:
+                            Console.Write("Podaj liczbę: ");
+                            int liczba = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Czy liczba jest pierwsza? " + Matematyka.CzyPierwsza(liczba));
+                            break;
+                        case 4:
+                            Console.Write("Podaj liczbę naturalną: ");
+                            int liczbaDoskonala = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Czy liczba jest doskonała? " + Matematyka.CzyDoskonala(liczbaDoskonala));
+                            break;
+                        case 5:
+                            Console.Write("Podaj pierwszą liczbę naturalną: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Podaj drugą liczbę naturalną: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("NWD wynosi: " + Matematyka.NWD(a, b));
+                            break;
+                        case 0:
+                            return;
+                        default:
+                            Console.WriteLine("Podaj liczbę od 0 do 5.");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Niewłaściwy format! Podaj liczbę całkowitą.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
