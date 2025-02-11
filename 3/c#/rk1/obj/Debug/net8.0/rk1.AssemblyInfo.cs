f483788a2a04c554d75561d87bd51405189957d1
ForcedBucket122 <dzejkob2112@gmail.com>
2025-02-11 12:43:06 +0100

    wychodze stad na serio

--- 3/c#/rk1/obj/Debug/net8.0/rk1.AssemblyInfo.cs
+++ 3/c#/rk1/obj/Debug/net8.0/rk1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rk1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1afc5777e7a2f8696d3e4dc7d864b256b9f394e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc8c42643252819a6b14ea14e2cd10e63968c98")]
 [assembly: System.Reflection.AssemblyProductAttribute("rk1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rk1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
