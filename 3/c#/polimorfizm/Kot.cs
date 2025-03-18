d58874ae72a74c2617fa46ed51fc2fc84ba11795
ForcedBucket122 <dzejkob2112@gmail.com>
2025-03-18 14:09:11 +0100

    cos

---
+++ 3/c#/polimorfizm/Kot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfizm
+{
+    internal class Kot:Zwierze
+    {
+        new public void DajGlos()
+        {
+            Console.WriteLine("glos: miau miau");
+        }
+        new public void Pozywienie()
+        {
+            Console.WriteLine("pozywienie: karma dla psa");
+        }
+        new public void Pozytek()
+        {
+            Console.WriteLine("Pozytek: lowie myszy");
+        }
+    }
+}
---
+++ 3/c#/polimorfizm/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace polimorfizm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bez polimorfizmu");
+            Kot kot1 = new Kot();
+
+            Zwierze kot2 = new Kot();
+
+            //kot1.DajGlos();
+            //kot1.Pozywienie();
+            //kot1.Pozytek();
+
+
+            //((Kot)kot2).DajGlos();
+            //((Kot)kot2).Pozywienie();
+            //((Kot)kot2).Pozytek();
+
+            List <Zwierze> list = new List <Zwierze>();
+            list.Add(kot1);
+            list.Add(kot2);
+            list.Add(new Kot());
+            list.Add(new Pies());
+            list.Add(new Pies());
+            list.Add(new Pies());
+            list.Add(new Krowa());
+            list.Add(new Krowa());
+            list.Add(new Krowa());
+
+            Console.WriteLine(list.Count());
+
+            //for (int i = 0; i < list.Count; i++) {
+            //    list[i].DajGlos();
+            //    list[i].Pozywienie();
+            //    list[i].Pozytek();
+            //}
+            foreach (var item in list)
+            {
+                if (item.GetType()==typeof(Kot))
+                {
+                    ((Kot)item).DajGlos();
+                    ((Kot)item).Pozywienie();
+                    ((Kot)item).Pozytek();
+                }
+                else if (item.GetType() == typeof(Pies))
+                {
+                    ((Pies)item).DajGlos();
+                    ((Pies)item).Pozywienie();
+                    ((Pies)item).Pozytek();
+                }
+                else if (item.GetType() == typeof(Krowa))
+                {
+                    ((Krowa)item).DajGlos();
+                    ((Krowa)item).Pozywienie();
+                    ((Krowa)item).Pozytek();
+                }
+
+            }
+        }
+    }
+}
