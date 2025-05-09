634b33b5e0a34a3c3f6c040afa6aacc43b324ca1
ForcedBucket122 <dzejkob2112@gmail.com>
2025-05-09 11:19:06 +0200

    cos

---
+++ 3/c#/odczytPliku/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace odczytPliku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string sciezka = "pliki/test.txt";
+                StreamReader streamReader = new StreamReader(sciezka);
+                Console.WriteLine(streamReader.ReadLine());
+                Console.WriteLine(streamReader.ReadLine());
+                streamReader.Close();
+                //Console.WriteLine("Podaj sciezke");
+                //string sciezka2 = Console.ReadLine();
+                //StreamReader reader = new StreamReader(sciezka2);
+                //Console.WriteLine(reader.ReadToEnd());
+                
+            }catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
